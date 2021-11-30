20d447e7abbcba553ed95a624ac002a09e2e850f
MaeleG <215052723@student.uj.ac.za>
2021-11-30 06:16:37 +0200

    My Class

---
+++ Class.cs
@@ -0,0 +1,23 @@
+using IT_Quiz.filehandler;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT_Quiz
+{
+    public partial class frmITQuiz : Form
+    {
+        List<Panel> listPanel = new List<Panel>();
+        double countercorrect = 0;
+        int countWrong = 0;
+        string[] Ques = new string[15];
+        int index = 0;
+	}
+}
